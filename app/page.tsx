7fac39834ca10103cac9b1f86ccec223b1dd23ec
anuj9631 <anuj98426@gmail.com>
2025-12-16 16:55:52 +0530

    fixed dark modd

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button>subscribe to us </Button>;
+  return <Button variant={"destructive"}>subscribe to us </Button>;
 }
