c8d8e09c4706b4734427e53f7a6b99978d31718c
anuj9631 <anuj98426@gmail.com>
2025-12-16 16:33:10 +0530

    added dark mode features

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>subscribe to us </div>;
+  return <Button>subscribe to us </Button>;
 }
