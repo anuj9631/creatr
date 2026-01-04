99256f9b5b93b42ccc2847f68dce090906ced859
anuj9631 <anuj98426@gmail.com>
2026-01-04 14:04:39 +0530

    convex query

--- app/dashboard/settings/page.jsx
+++ app/dashboard/settings/page.jsx
@@ -1,6 +1,16 @@
+"use client"
+
 import React from 'react'
+import { api } from "@/convex/_generated/api";
+import { useQuery} from "convex/react";
 
 const SettingsPage = () => {
+
+
+   const data = useQuery(
+    api.users.getCurrentUser
+  );
+  console.log(data);
   return (
     <div>SettingsPages</div>
   )
